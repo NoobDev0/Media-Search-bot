9f93fd28ed9b6f20404fd3c91fa365821c98bc11
Noob Dev <88297858+NoobDev0@users.noreply.github.com>
2021-10-16 20:52:06 +0530

    Add files via upload

---
+++ plugins/translation.py
@@ -0,0 +1,33 @@
+class Text(object):
+    
+    START_TEXT = """<u><b>Hey {}!!</b></u>
+<i>What's Up '<b>Film Buff!</b>'🎬</i>\n\n ᴀᴍ ᴊᴜsᴛ ᴀ ᴀᴅᴠᴀɴᴄᴇ ᴀᴜᴛᴏ ꜰɪʟᴛᴇʀ ʙᴏᴛ.... ᴡɪᴛʜ ᴍᴀɴʏ ᴘᴏᴡᴇʀs!🔥
+ 
+<b>ɪ ᴀᴍ ᴄʀᴇᴀᴛᴇᴅ ᴛᴏ sᴇʀᴠᴇ <u>ᗩՏK ᗩ TO ᘔ ᗰOᐯIᗴՏ</u></b>🎊 
+🎭Click The <b>Help</b> To See And Unlock My Full Potential! \n<i>Join Our Official Channel</i>
+@CinemaHaunter 
+</i>""" 
+
+    FEATURES_TEXT = """⚡<b>Given Below are Some Features With Which You Can Mess Me</b>⚡
+
+@CinemaHaunter
+"""
+    AUTOFILTER_TEXT = """ <b>Auto Filter Searches the Database And Sends the File **Privately** (This Works in Groups) with IMDb Info</b>
+    
+@CinemaHaunter 
+"""
+    PM_TEXT = """<b>Pm Filter Searches the Database And Sends the Links Of the Files <i>Without</i> Requesting In the Group!
+Just Send your Query **Privately** to thr Bot</b>
+
+@CinemaHaunter 
+"""
+    INLINE_TEXT = """<b>Inline Mode is available for this Bot!
+Type In the Username of This Bot and See the Magic!</b>
+
+@CinemaHaunter 
+"""
+    VIDEO_TEXT = """<b>This is Used to Stream Videos in Telegram Voice Chat For Both Channels And Groups!
+Supports live streams, YouTube Videos and Telegram Media. With Record Stream Support, Schedule Streams, and Many More!</b>
+
+@CinemaHaunter 
+"""
